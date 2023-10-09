b90b70764cce7238989374f654bcf467de736199
lmueller555 <129226598+lmueller555@users.noreply.github.com>
2023-10-09 15:42:00 -0400

    Update llm_app_dev.py

--- llm_app_dev.py
+++ llm_app_dev.py
@@ -19,9 +19,11 @@ PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
 INDEX_NAME = "firstindex"
 NAMESPACE = "book"
 
+print("Initializing OpenAI Embeddings...")
 # Set up OpenAI Embeddings
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
+print("Initializing Pinecone...")
 # Initialize Pinecone
 pinecone.init(
     api_key=PINECONE_API_KEY,
@@ -29,6 +31,7 @@ pinecone.init(
     index = INDEX_NAME
 )
 
+print("Connecting to Pinecone index...")
 # Connect to an existing Pinecone index
 docsearch = Pinecone.from_existing_index(INDEX_NAME, embeddings, namespace=NAMESPACE)
 
@@ -39,35 +42,30 @@ llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
 chain = load_qa_chain(llm, chain_type="stuff")
 
 # Streamlit UI
-st.title("WGU ChatLR Pre-Alpha DEV")
+st.title("WGU ChatLR Pre-Alpha Test DEV")
 
 raw_user_query = st.text_input("What would you like to know?")
-instruction = "You are an expert tutor, and you are never allowed to answer based on anything but the documents that were uploaded. If the answer is not in the uploaded books, just say that you don't know the answer."
 
-# Check for specific conditions in the user's query
-if "summarize" in raw_user_query.lower():
-    instruction += " Please provide a concise summary of the relevant information."
+if raw_user_query:  # Checking if there's input to process
+    instruction = "You are not allowed to answer based on anything but the documents that were uploaded."
+    if "summarize" in raw_user_query.lower():
+        instruction += "You are an expert tutor. You are not allowed to answer based on anything but the documents that were uploaded."
+    user_query = f"{instruction} {raw_user_query}"
 
-user_query = f"{instruction} {raw_user_query}"
+    print(f"Received user query: {user_query}")
+    
+    # Perform a similarity search based on the user's query
+    print("Performing similarity search...")
+    docs = docsearch.similarity_search(user_query, namespace=NAMESPACE)
+    # st.write(f"Debug: Documents from Similarity Search = {docs}")
 
-if st.button("Submit"):
-    if user_query:
+    # Use the chain to get an answer to the user's query using the retrieved documents.
+    response = chain.run(input_documents=docs, question=user_query)
+    # st.write(f"Debug: Raw Response from LLM = {response}")
 
-        # Perform a similarity search based on the user's query
-        docs = docsearch.similarity_search(user_query, namespace=NAMESPACE)
-
-        # Debug Output: Documents from Similarity Search (this might provide insights into the vectors being used)
-        # st.write(f"Debug: Documents from Similarity Search = {docs}")
-
-        # Use the chain to get an answer to the user's query using the retrieved documents.
-        response = chain.run(input_documents=docs, question=user_query)
-
-        # Debug Output: Raw Response from LLM
-        # st.write(f"Debug: Raw Response from LLM = {response}")
-
-        st.write(f"Answer: {response}")
-    else:
-        st.write("Please enter a valid question.")
+    st.write(f"Answer: {response}")
+else:
+    st.write("Please enter a valid question.")
 
 if __name__ == "__main__":
     pass  # Streamlit runs the script top to bottom every time an action is taken, no need for a main loop.
